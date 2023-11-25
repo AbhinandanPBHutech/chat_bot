12bb7d68676b5ed0b4c2ea06d2e68730cf4f54c9
AbhinandanPBHutech <110391242+AbhinandanPBHutech@users.noreply.github.com>
2023-11-25 13:07:20 +0530

    fixes

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -1,26 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import axios from 'axios';
+import axios from "axios";
 
 export default async function handler(req, res) {
   const referer = req.headers.referer || req.headers.referrer; // get the referer from the request headers
 
-  if (req.method !== 'POST') {
-    res.status(405).json({ message: 'Method should be POST' });
-  } else if (process.env.NODE_ENV !== "development") {
-    if (!referer || referer !== process.env.APP_URL) {
-      res.status(401).json({ message: 'Unauthorized' });
-    }
-  }
-  else {
+  if (req.method !== "POST") {
+    res.status(405).json({ message: "Method should be POST" });
+  } else {
     try {
       const { body } = req;
-      const url = 'https://api.openai.com/v1/chat/completions';
+      const url = "https://api.openai.com/v1/chat/completions";
       const headers = {
-        'Content-type': 'application/json',
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+        "Content-type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
       };
 
-      const response = await axios.post(url, body, { headers: headers })
+      const response = await axios.post(url, body, { headers: headers });
 
       res.status(200).json(response.data);
     } catch (error) {
@@ -28,5 +23,4 @@ export default async function handler(req, res) {
       res.status(500).json({ message: "Something went wrong" });
     }
   }
-  
 }
